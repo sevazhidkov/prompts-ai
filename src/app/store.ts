895d7e78d303dbd3254f358e8240399fa57dda81
Seva Zhidkov <seva@zhidkoff.com>
2020-07-27 16:12:23 +0300

    save state in local storage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,27 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import counterReducer from '../components/counter/counterSlice';
 import editorReducer from './slices/editorSlice';
 
+const reducers = combineReducers(
+    {
+      counter: counterReducer,
+      editor: editorReducer
+    }
+);
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    editor: editorReducer,
-  },
+  reducer: persistedReducer
 });
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
+import { store, persistor } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')

de090951f64d3d574c81f13359e26faeea6f47be
Seva Zhidkov <seva@zhidkoff.com>
2020-08-11 13:36:24 +0300

    optimize imports in mode tabs

--- src/components/modeTabs/ModeTabs.tsx
+++ src/components/modeTabs/ModeTabs.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
+import {Grid, Hidden, AppBar, Tabs, Tab, Typography, Box} from '@material-ui/core';
 import { makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import ExampleModeTab from "../exampleModeTab/ExampleModeTab";
 import VariationsTab from '../creativeModTab/VariationsTab';
 import {useDispatch, useSelector} from "react-redux";
 import {selectTabIndex, updateTabIndex, TabIndex} from "../../app/slices/editorSlice";
-import {Grid, Hidden} from '@material-ui/core';
 import CodeGeneratorButton from '../codeGeneratorButton/CodeGeneratorButton';
 import ChatBotMode from '../chatBotMode/ChatBotMode';
 
